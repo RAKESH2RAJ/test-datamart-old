7bb0aad7e33abe3d7d092501e3c77ec2fbd0231a
sachalic-data <sai.tivoli8@gmail.com>
2021-01-27 08:07:16 +0530

    com

--- com/pg/source-data-loading.py
+++ com/pg/source-data-loading.py
@@ -6,17 +6,6 @@ import utils.aws_utils as ut
 
 if __name__ == '__main__':
 
-    os.environ["PYSPARK_SUBMIT_ARGS"] = (
-        '--packages "mysql:mysql-connector-java:8.0.15" pyspark-shell'
-    )
-
-    # Create the SparkSession
-    spark = SparkSession \
-        .builder \
-        .appName("Read ingestion enterprise applications") \
-        .getOrCreate()
-    spark.sparkContext.setLogLevel('ERROR')
-
     current_dir = os.path.abspath(os.path.dirname(__file__))
     app_config_path = os.path.abspath(current_dir + "/../../" + "application.yml")
     app_secrets_path = os.path.abspath(current_dir + "/../../" + ".secrets")
@@ -25,6 +14,14 @@ if __name__ == '__main__':
     app_conf = yaml.load(conf, Loader=yaml.FullLoader)
     secret = open(app_secrets_path)
     app_secret = yaml.load(secret, Loader=yaml.FullLoader)
+
+    # Create the SparkSession
+    spark = SparkSession \
+        .builder \
+        .appName("Read ingestion enterprise applications") \
+        .config("spark.mongodb.input.uri", app_secret["mongodb_config"]["uri"])\
+        .getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
 
     src_list = app_conf['source_list']
     for src in src_list:
