df757056fc44c53daa97a88b993a6dd67e783409
sachalic-data <sai.tivoli8@gmail.com>
2021-02-03 08:06:47 +0530

    refactoring code

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -34,7 +34,7 @@ if __name__ == '__main__':
             cp_df.createOrReplaceTempView(tgt_conf["source_data"])
             cp_df.show(5, False)
 
-            regis_dim_df = spark.sql(
+            regis_dim_df = spark.sql("""
                 SELECT
                     CUSTOMERS.FN_UUID() AS REGIS_KEY, REGIS_CNSM_ID AS CNSM_ID,REGIS_CTY_CODE AS CTY_CODE,
                     REGIS_ID, REGIS_DATE, REGIS_LTY_ID AS LTY_ID, REGIS_CHANNEL, REGIS_GENDER, REGIS_CITY, INS_TS
@@ -47,7 +47,7 @@ if __name__ == '__main__':
                     WHERE
                         CAST(INS_TS AS DATE) = CURRENT_DATE
                     )
-                CP)
+                CP""")
             regis_dim_df.show(5, False)
 
 
