6aa97fd553bf4a3e09b924caa3ce284a46ed426d
sachalic-data <sai.tivoli8@gmail.com>
2021-02-03 08:50:28 +0530

    code fixing

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -42,20 +42,7 @@ if __name__ == '__main__':
             cp_df.createOrReplaceTempView(tgt_conf["source_data"])
             cp_df.show(5, False)
 
-            regis_dim_df = spark.sql("""
-                SELECT
-                    fn_uuid() AS REGIS_KEY, REGIS_CNSM_ID AS CNSM_ID,REGIS_CTY_CODE AS CTY_CODE,
-                    REGIS_ID, REGIS_DATE, REGIS_LTY_ID AS LTY_ID, REGIS_CHANNEL, REGIS_GENDER, REGIS_CITY, ins_date
-                FROM
-                    (SELECT
-                        DISTINCT REGIS_CNSM_ID, CAST(REGIS_CTY_CODE AS SMALLINT), CAST(REGIS_ID AS INTEGER),
-                        REGIS_LTY_ID, REGIS_DATE, REGIS_CHANNEL, REGIS_GENDER, REGIS_CITY, ins_date
-                    FROM
-                        CP
-                    WHERE
-                        ins_date = CURRENT_DATE
-                    )
-                CP""")
+            regis_dim_df = spark.sql("""tgt_conf["loading_query"]""")
             regis_dim_df.show(5, False)
 
 
