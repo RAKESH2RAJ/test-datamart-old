af08b54d43b40f7c78855c9bb389a737f0a0ab9d
sachalic-data <sai.tivoli8@gmail.com>
2021-02-15 09:25:05 +0530

    code fixing

--- com/pg/target_data_loading.py
+++ com/pg/target_data_loading.py
@@ -68,17 +68,17 @@ if __name__ == '__main__':
                 src_df = spark.read.parquet(stg_loc + "/" + src)
                 src_df.show()
                 src_df.createOrReplaceTempView(src)
-            src_table = app_conf['RTL_TXN_FCT']['source_table']
-            jdbc_url = ut.get_redshift_jdbc_url(app_secret)
-                txn_df = spark.read\
-                    .format("io.github.spark_redshift_community.spark.redshift")\
-                    .option("url", jdbc_url) \
-                    .option("query", app_conf["REGIS_DIM"]["loading_query"]) \
-                    .option("forward_spark_s3_credentials", "true")\
-                    .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp")\
-                    .load()
+    src_table = app_conf['RTL_TXN_FCT']['source_table']
+    jdbc_url = ut.get_redshift_jdbc_url(app_secret)
+    txn_df = spark.read\
+        .format("io.github.spark_redshift_community.spark.redshift")\
+        .option("url", jdbc_url) \
+        .option("query", app_conf["REGIS_DIM"]["loading_query"]) \
+        .option("forward_spark_s3_credentials", "true")\
+        .option("tempdir", "s3a://" + app_conf["s3_conf"]["s3_bucket"] + "/temp")\
+        .load()
 
-                txn_df.show(5, False)
+    txn_df.show(5, False)
 
 
 # spark-submit --executor-memory 5G --driver-memory 5G --executor-cores 3 --jars "https://s3.amazonaws.com/redshift-downloads/drivers/jdbc/1.2.36.1060/RedshiftJDBC42-no-awssdk-1.2.36.1060.jar" --master yarn --packages "io.github.spark-redshift-community:spark-redshift_2.11:4.0.1,org.apache.spark:spark-avro_2.11:2.4.2,org.apache.hadoop:hadoop-aws:2.7.4,org.apache.hadoop:hadoop-aws:2.7.4" com/pg/target_data_loading.py
